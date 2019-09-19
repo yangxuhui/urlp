26695d61f52b5161b316eeb5934a874c7e5901c9
yangxuhui <yangxuhui1992@163.com>
2019-09-19 13:46:58 +0800

    Add setup.py and setup.cfg

---
+++ setup.py
@@ -0,0 +1,49 @@
+"""A setuptools based setup module.
+See:
+https://packaging.python.org/guides/distributing-packages-using-setuptools/
+https://github.com/pypa/sampleproject
+"""
+
+
+from setuptools import setup, find_packages
+from os import path
+from io import open
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+
+setup(
+    name='urlp',
+    version='0.0.0',
+    description='A simple command-line utility for parsing URLs.',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/yangxuhui/urlp',
+    author='Xuhui Yang',
+    author_email='yangxuhui1992@163.com', 
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Utilities',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+    ],
+    keywords='url parse command-line',
+    packages=['urlp'],
+    python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*, <4',
+    install_requires=['tldextract'],
+    entry_points={
+        'console_scripts': [
+            'urlp=urlp.cli:main',
+        ],
+    },
+)
